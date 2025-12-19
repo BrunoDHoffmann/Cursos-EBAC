57bb877fadd7f7f9e09f41c857ca1917cd88d0db
bergsonall <bruninhodubal@hotmail.com>
2025-12-19 19:26:53 -0300

    exercicio algoritmo, identificar numero que aparece a mesma quantidade de vezes que seu valor

---
+++ back-end/algoritmos/n_valor_freq_igual.py
@@ -0,0 +1,6 @@
+from collections import Counter
+array = [1, 1, 2, 2, 3, 4, 5, 6, 7, 7]
+
+contador = Counter(array)
+sortudo = [item for item, count in contador.items() if item == count]
+print(sortudo)
