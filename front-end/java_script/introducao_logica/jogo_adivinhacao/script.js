10de95a84e8f50e93a2cd27fb8443ab705653b6f
bergsonall <bruninhodubal@hotmail.com>
2026-01-08 18:44:37 -0300

    finalização jogo advinhação

--- front-end/java_script/introducao_logica/jogo_adivinhacao/script.js
+++ front-end/java_script/introducao_logica/jogo_adivinhacao/script.js
@@ -1,17 +1,49 @@
+let tentativas = 0;
+let numeroSecreto = Math.floor(Math.random() * 100) + 1;
+const maxTentativas = 10;
+
 document.addEventListener('DOMContentLoaded', () => {
-    const numeroSecreto = Math.floor(Math.random() * 100) +1;
-    let tentativas = 0;
-    const maxTentativas = 10;
+    document.getElementById('btn-chutar').addEventListener('click', () => {
+        tentativas++, validaResultado(numeroSecreto, tentativas, maxTentativas)
+    });
+
+    document.getElementById('btn-reset').addEventListener('click', () => { resetGame() })
 });
 
-function validaResultado(){
+function resetGame() {
+    tentativas = 0;
+    document.getElementById('dica').style.display = 'none';
+    document.getElementById('tentativas').style.display = 'none';
+    numeroSecreto = Math.floor(Math.random() * 100) + 1;
+    document.getElementById('btn-chutar').disabled = false
+}
+
+function validaResultado(numeroSecreto, tentativas, maxTentativas) {
     const chute = parseInt(document.getElementById('palpite').value);
-    console.log(numero)
+    console.log(numeroSecreto, tentativas, maxTentativas)
+    document.getElementById('tentativas').textContent = `TENTATIVAS: ${tentativas}`
+    document.getElementById('tentativas').style.display = 'block'
+    if (tentativas >= maxTentativas) {
+        document.getElementById('dica').textContent = 'Numero maximo de tentativas atingido.'
+        document.getElementById('dica').style.display = 'block'
+        document.getElementById('btn-chutar').disabled = true
+        return
+    }
     if (!chute) {
         document.getElementById('dica').textContent = 'Digite seu palpite!'
         document.getElementById('dica').style.display = 'block'
-    } else if (chute < numeroSecreto){
+    } else if (chute < 0 || chute > 100) {
+        document.getElementById('dica').textContent = 'Digite um valor entre 0 e 100!'
+        document.getElementById('dica').style.display = 'block'
+    } else if (chute < numeroSecreto && chute >= 0) {
         document.getElementById('dica').textContent = 'Tente um numero maior.'
         document.getElementById('dica').style.display = 'block'
+    } else if (chute > numeroSecreto && chute <= 100) {
+        document.getElementById('dica').textContent = 'Tente um numero menor.'
+        document.getElementById('dica').style.display = 'block'
+    } else if (chute === numeroSecreto) {
+        document.getElementById('dica').textContent = 'ACERTOU!'
+        document.getElementById('dica').style.display = 'block'
+        document.getElementById('btn-chutar').disabled = true
     }
 }
